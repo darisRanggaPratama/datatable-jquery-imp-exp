40e0e373b0d4b299a5703dfdbf1e152e1f1ec5d0
raden rangga pratama <daris.rangga.pratama@gmail.com>
2024-11-26 06:21:14 +0700

    initial commit

--- view/script.js
+++ view/script.js
@@ -33,7 +33,10 @@ $(document).ready(function() {
                     `;
                 }
             }
-        ]
+        ],
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.13.7/i18n/id.json"
+        }
     });
 
     // Create Data
